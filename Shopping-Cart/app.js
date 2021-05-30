4136e04801fa7e357478d467aaab2d0d089a269c
Edward <edwardn2147@yahoo.com>
2021-05-29 21:02:45 -0700

    Retrieve furniture data from Contentful

--- Shopping-Cart/app.js
+++ Shopping-Cart/app.js
@@ -1,3 +1,10 @@
+const client = contentful.createClient({
+    // This is the space ID. A space is like a project folder in Contentful terms
+    space: "spg21isy7pkr",
+    // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+    accessToken: "dGW7m0YctrD9wiWDXAmDo9s0_ENnT_1Ird3GOuuiVnM"
+});
+
 // variables
 const cartBtn = document.querySelector('.cart-btn');
 const closeCartBtn = document.querySelector('.close-cart');
@@ -18,9 +25,15 @@ let buttonsDOM = [];
 class Products {
     async getProducts(){
         try {
-            let result = await fetch('products.json');
-            let data = await result.json();
-            let products = data.items;
+
+            let contentful = await client.getEntries({
+                content_type: 'comfyHouseProducts'
+            });
+
+            // let result = await fetch('products.json');
+            // let data = await result.json();
+
+            let products = contentful.items;
             products = products.map(item => {
                 const {title, price} = item.fields;
                 const {id} = item.sys;
